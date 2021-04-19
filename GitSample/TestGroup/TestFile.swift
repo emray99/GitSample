7944e56190263f418b3f4973ad65f624daac60e6
Ray Liu <emray@vt.edu>
2021-04-18 21:13:35 -0400

    Added a testGroup and a testFile

---
+++ GitSample/TestGroup/TestFile.swift
@@ -0,0 +1,20 @@
+//
+//  TestFile.swift
+//  GitSample
+//
+//  Created by Ray Liu on 4/18/21.
+//
+
+import SwiftUI
+
+struct TestFile: View {
+    var body: some View {
+        Text("This is a test file")
+    }
+}
+
+struct TestFile_Previews: PreviewProvider {
+    static var previews: some View {
+        TestFile()
+    }
+}
